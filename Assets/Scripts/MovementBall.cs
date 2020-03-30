b298c44404ae587d117b5755027bc20a4e1b0a54
Bryann Alfaro <alf19372@uvg.edu.gt>
2020-03-30 14:22:36 -0600

    first commit

---
+++ Assets/Scripts/MovementBall.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementBall : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float force = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump();
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (rb)
+        {
+            rb.AddForce(Input.GetAxis("Horizontal") * force, 0, Input.GetAxis("Vertical") * force);
+        }
+    }
+
+    private void jump()
+    {
+        if (rb)
+        {
+            if (Mathf.Abs(rb.velocity.y) < 0.0005f)
+            {
+                rb.AddForce(0, force, 0, ForceMode.Impulse);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    public float anguloRotacion = 45;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(anguloRotacion, 0, 0) * Time.deltaTime);
+    }
+}
